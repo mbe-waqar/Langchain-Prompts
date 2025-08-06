07b69a510cb3f765817c2edeeac77d2991aa16a1
Waqar Ali <waqar0343930@gmail.com>
2025-08-06 12:43:29 +0500

    initial commits

---
+++ prompt_ui.py
@@ -0,0 +1,13 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import streamlit as st
+
+load_dotenv()
+
+st.header("Reasearch Tool")
+
+user_input = st.text_input("Enter your question or prompt")
+
+if st.button("Summarize"):
+    result = model.invoke(user_input)
+    st.write(result.content)
