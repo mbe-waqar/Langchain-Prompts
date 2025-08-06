3d8ccd3e2ca6625d74769004dfd8c0b2153b598b
Waqar Ali <waqar0343930@gmail.com>
2025-08-06 13:50:37 +0500

    simple chatbot

--- chatbot.py
+++ chatbot.py
@@ -1,19 +1,22 @@
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from dotenv import load_dotenv
 
 load_dotenv()
 
 model = ChatOpenAI()
 
-chat_history = []
+chat_history = [
+    SystemMessage(content="you are a helpful AI assitant")
+]
 
 while True:
     user_input = input("You: ")
-    chat_history.append(user_input)
+    chat_history.append(HumanMessage(content=user_input))
     if user_input == "quit":
         break
     result = model.invoke(user_input)
-    chat_history.append(result.content)
+    chat_history.append(AIMessage(content=result.content))
     print("AI: ", result.content)
 
 print(chat_history)
